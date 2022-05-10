c853791d74af3bb7c756b568d9dd65b3501d87a7
sseung12 <pon02129@naver.com>
2022-05-10 22:27:41 +0900

    [Add] 유저 기본생성시 기본 프로필 이미지 추가

--- src/main/java/com/numble/shortForm/user/entity/Users.java
+++ src/main/java/com/numble/shortForm/user/entity/Users.java
@@ -53,11 +53,12 @@ public class Users extends BaseTime implements UserDetails {
     private List<String> roles = new ArrayList<>();
 
     @Builder
-    public Users(String email, String password, String nickname, List<String> roles) {
+    public Users(String email, String password, String nickname, List<String> roles,ProfileImg profileImg) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.roles = roles;
+        this.profileImg =profileImg;
     }
 
     @Override
