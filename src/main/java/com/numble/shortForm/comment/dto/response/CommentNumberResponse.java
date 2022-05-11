2dd88aef1e3d3fe7c1dad4cd72cb3ff46a27a5a2
이승수 <66988341+sseung12@users.noreply.github.com>
2022-05-12 01:22:12 +0900

    Revert "Revert "Revert "Feature comment"""

--- src/main/java/com/numble/shortForm/comment/dto/response/CommentNumberResponse.java
+++ src/main/java/com/numble/shortForm/comment/dto/response/CommentNumberResponse.java
@@ -4,16 +4,12 @@ import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-
 @Getter
+@Setter
 public class CommentNumberResponse {
 
     private long id;
     private String nickname;
-
     private String context;
     private String title;
     private boolean isBlock;
@@ -21,18 +17,11 @@ public class CommentNumberResponse {
     private Long commentSeq;
     private Long videoId;
     private Long commentCount;
-    private LocalDate created_at;
-    private Long LikeCount;
-    private String ProfileImgUrl;
-
-    @Setter
     private boolean isReComment;
 
     @QueryProjection
     public CommentNumberResponse(long id, String nickname, String context, String title, boolean isBlock, Long userId,
-                                 Long commentSeq, Long videoId, Long commentCount, LocalDateTime created_at, Long LikeCount,
-                                 String ProfileImgUrl)
-    {
+                                 Long commentSeq, Long videoId, Long commentCount) {
         this.id = id;
         this.nickname = nickname;
         this.context = context;
@@ -42,8 +31,5 @@ public class CommentNumberResponse {
         this.commentSeq = commentSeq;
         this.videoId = videoId;
         this.commentCount = commentCount;
-        this.created_at = created_at.toLocalDate();
-        this.LikeCount = LikeCount;
-        this.ProfileImgUrl = ProfileImgUrl;
     }
 }
