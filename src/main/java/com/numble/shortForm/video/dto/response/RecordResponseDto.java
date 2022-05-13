f90e955e0825bd3ae6aba838cde7e33cd0efb754
sseung12 <pon02129@naver.com>
2022-05-13 17:42:13 +0900

    [Add] RecordVideo 구조

---
+++ src/main/java/com/numble/shortForm/video/dto/response/RecordResponseDto.java
@@ -0,0 +1,13 @@
+package com.numble.shortForm.video.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecordResponseDto {
+
+    private Long videoId;
+
+
+}
---
+++ src/main/java/com/numble/shortForm/video/repository/RecordCustomRepository.java
@@ -0,0 +1,10 @@
+package com.numble.shortForm.video.repository;
+
+import com.numble.shortForm.video.entity.RecordVideo;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface RecordCustomRepository {
+
+}
