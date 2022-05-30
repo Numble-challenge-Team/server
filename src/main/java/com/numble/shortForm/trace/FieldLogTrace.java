7f5c2367f042f14a82df2304dc2510941d026763
sseung12 <pon02129@naver.com>
2022-05-30 21:43:40 +0900

    [Add] trace 로직

--- src/main/java/com/numble/shortForm/trace/FieldLogTrace.java
+++ src/main/java/com/numble/shortForm/trace/FieldLogTrace.java
@@ -1,41 +1,42 @@
 package com.numble.shortForm.trace;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-public class TraceV1 {
+public class FieldLogTrace implements LogTrace{
 
     private static final String START_PREFIX ="-->";
     private static final String COMPLETE_PREFIX="<--";
     private static final String EX_PREFIX ="<X-";
 
+    // traceId 동기화,동시성 이슈 발생
+    private ThreadLocal<TraceId> traceIdHolder =new ThreadLocal<>();
+    @Override
     public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceIdHolder.get();
 
-        TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}",traceId.getId(),addSpace(START_PREFIX,traceId.getLevel()),message );
         return new TraceStatus(traceId,startTimeMs,message);
     }
 
-    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
-        TraceId nextId = beforeTraceId.createNextId();
-        Long startTimeMs = System.currentTimeMillis();
-        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX,
-                nextId.getLevel()) + message);
-
-        return new TraceStatus(nextId, startTimeMs, message);
+    private void syncTraceId() {
+        if (traceIdHolder.get() == null) {
+            traceIdHolder.set(new TraceId());
+        }else {
+            traceIdHolder.set(traceIdHolder.get().createNextId());
+        }
     }
 
+    @Override
+    public void end(TraceStatus status) {
 
-    public void end(TraceStatus traceStatus) {
-        complete(traceStatus,null);
     }
 
-
+    @Override
     public void exception(TraceStatus status, Exception e) {
-        complete(status,e);
+
     }
 
     private void complete(TraceStatus status, Exception e) {
@@ -47,9 +48,20 @@ public class TraceV1 {
                     traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms");
         } else {
             log.info("[" + traceId.getId() + "] " + addSpace(EX_PREFIX,
-                            traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms" +
-                            " ex=" + e);
-        } }
+                    traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms" +
+                    " ex=" + e);
+
+        }
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.get().isFirstLevel()) {
+            traceIdHolder.remove();
+        }else{
+            traceIdHolder.set(traceIdHolder.get().createPreviousId());
+        }
+    }
 
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
